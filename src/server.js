4de627dffcc140842b5a44d7c9957a272593a4dc
Gabriel <gabrielestevo92@gmail.com>
2024-03-30 20:57:18 -0300

    add express

--- src/server.js
+++ src/server.js
@@ -6,12 +6,12 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
-
+const express = require('express')
 
 const cert = fs.readFileSync(
     path.resolve(__dirname, `../certs/${process.env.GN_CERT}`)
   );
-  
+
 const agent = new https.Agent({
   pfx: cert,
   passphrase: ''
@@ -23,50 +23,65 @@ const credentials = Buffer.from(
     `${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
 ).toString('base64');
 
-axios({
-    method: 'POST',
-    url: `${process.env.GN_ENDPOINT}/oauth/token`,
-    headers: {
-        Authorization: `Basic ${credentials}`,
-        'Content-Type': 'application/json'
-    },
-    httpsAgent: agent,
-    data: {
-        grant_type: 'client_credentials'
-    }
-}).then((response) => {
-    const acessToken = response.data.access_token;
+const app = express();
 
-    const reqGN = axios.create({
-        baseURL: process.env.GN_ENDPOINT,
-        httpsAgent: agent,
+app.set('view engine', 'ejs');
+app.set('views', 'src/views')
+
+app.get('/', (req,res) => {
+    axios({
+        method: 'POST',
+        url: `${process.env.GN_ENDPOINT}/oauth/token`,
         headers: {
-            Authorization: `Bearer ${acessToken}`,
+            Authorization: `Basic ${credentials}`,
             'Content-Type': 'application/json'
+        },
+        httpsAgent: agent,
+        data: {
+            grant_type: 'client_credentials'
         }
+        }).then((response) => {
+            const acessToken = response.data.access_token;
+
+            const reqGN = axios.create({
+                baseURL: process.env.GN_ENDPOINT,
+                httpsAgent: agent,
+                headers: {
+                    Authorization: `Bearer ${acessToken}`,
+                    'Content-Type': 'application/json'
+                }
+            })
+
+
+
+            const dataCob = {
+                calendario: {
+                    expiracao: 3600
+                },
+                devedor: {
+                    cpf: '09745291412',
+                    nome: 'Gabriel Estevão Nunes'
+                },
+                valor: {
+                    original: '5.00'
+                },
+                chave: '83986732193',
+                solicitacaoPagador: 'Informe o número ou identificador do pedido.'
+            }
+
+
+            reqGN.post('/v2/cob', dataCob).then((response) => res.send(response.data))
+        
     })
 
 
-
-    const dataCob = {
-        calendario: {
-            expiracao: 3600
-        },
-        devedor: {
-            cpf: '09745291412',
-            nome: 'Gabriel Estevão Nunes'
-        },
-        valor: {
-            original: '5.00'
-        },
-        chave: '83986732193',
-        solicitacaoPagador: 'Informe o número ou identificador do pedido.'
-    }
-
-
-    reqGN.post('/v2/cob', dataCob).then((response) => console.log(response.data))
-    
 })
+
+
+app.listen(8000, () => {
+    console.log("running")
+})
+
 
 
 
